a62df04b38fbfb5410b6975b224e3d4858d58dc2
eksana <ser.vajnrub@yandex.ru>
2016-08-18 13:22:22 +0400

    ch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp'),
 scss = require('gulp-scss'),
  browserSync = require('browser-sync'); // Подключаем Browser Sync
+ concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+ uglify = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
+
+
+
 
 gulp.task('scss', function(){ // Создаем таск "scss"
     return gulp.src('app/scss/**/*.scss') // Берем источник
         .pipe(scss()) // Преобразуем Scss в CSS посредством gulp-scss
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице 
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([ // Берем все необходимые библиотеки
+		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+		])
+		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+		.pipe(uglify()) // Сжимаем JS файл
+		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
 });
 
 gulp.task('watch', function() {
